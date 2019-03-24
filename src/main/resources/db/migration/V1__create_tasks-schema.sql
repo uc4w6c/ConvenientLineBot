ab2447e93dc3b75609151032a4dbbdcd9779f3be
Yuta <uc4w6c@bma.biglobe.ne.jp>
2019-03-24 22:28:54 +0900

    IDの自動採番方法が異なっていたため修正

--- src/main/resources/db/migration/V1__create_tasks-schema.sql
+++ src/main/resources/db/migration/V1__create_tasks-schema.sql
@@ -1,2 +1,2 @@
-CREATE table tasks(id int primary key AUTO_INCREMENT, source_type integer, user_id varchar(50), group_id varchar(50), room_id varchar(50), todo_text varchar(150), created_at time, created_at time);
-CREATE table states(id int primary key AUTO_INCREMENT, source_type integer, user_id varchar(50), group_id varchar(50), room_id varchar(50), state_kind integer, status integer, updated_at time);
+CREATE table tasks(id SERIAL primary key, source_type integer, user_id varchar(50), group_id varchar(50), room_id varchar(50), todo_text varchar(150), created_at time, created_at time);
+CREATE table states(id SERIAL primary key, source_type integer, user_id varchar(50), group_id varchar(50), room_id varchar(50), state_kind integer, status integer, updated_at time);
