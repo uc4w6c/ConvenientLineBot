b6919f36c9f376533932a89e4d98277662b454e7
Yuta <uc4w6c@bma.biglobe.ne.jp>
2019-03-25 20:44:57 +0900

    Stateのupdated_atを自動で入力するように修正

--- src/main/java/com/herokuapp/convenient/domain/State.java
+++ src/main/java/com/herokuapp/convenient/domain/State.java
@@ -9,6 +9,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
+import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 
 import lombok.AllArgsConstructor;
@@ -39,6 +40,7 @@ public class State {
 
 	private final int status;
 
+	@CreatedDate
 	@LastModifiedDate
 	private LocalTime updatedAt;
 
