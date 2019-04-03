5f8404ff6f9120acb8e67fba1a6679cc398ab57b
Yuta <uc4w6c@bma.biglobe.ne.jp>
2019-04-03 22:43:13 +0900

    ConvenientLineBotApplicationに@Componentを追加

--- src/main/java/com/herokuapp/convenient/ConvenientLineBotApplication.java
+++ src/main/java/com/herokuapp/convenient/ConvenientLineBotApplication.java
@@ -4,9 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.convert.threeten.Jsr310JpaConverters;
+import org.springframework.stereotype.Component;
 
 @SpringBootApplication
 @EntityScan(basePackageClasses = {ConvenientLineBotApplication.class, Jsr310JpaConverters.class})
+@Component
 public class ConvenientLineBotApplication {
 
 	public static void main(String[] args) {
