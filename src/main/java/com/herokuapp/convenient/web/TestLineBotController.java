b778d4fc38017d9d04259bfad46be31ce6cd0310
Yuta <uc4w6c@bma.biglobe.ne.jp>
2019-04-10 00:06:41 +0900

    ローカルで動く環境がやっとできた

--- src/main/java/com/herokuapp/convenient/web/TestLineBotController.java
+++ src/main/java/com/herokuapp/convenient/web/TestLineBotController.java
@@ -8,6 +8,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 import com.herokuapp.convenient.service.LineBotService;
+import com.linecorp.bot.model.event.CallbackRequest;
+import com.linecorp.bot.model.event.Event;
 import com.linecorp.bot.model.event.MessageEvent;
 import com.linecorp.bot.model.event.message.TextMessageContent;
 import com.linecorp.bot.model.message.Message;
@@ -23,9 +25,21 @@ public class TestLineBotController {
 	}
 
 	@PostMapping(path = "create")
-	public Message create(@RequestBody MessageEvent<TextMessageContent> event) {
-		System.out.println(event);
-		return lineBotService.makeReply(event);
+	public Message create(@RequestBody CallbackRequest callBackRequest) {
+		System.out.println(callBackRequest);
+
+		// TODO: 色々汚いから変えたい
+		// 暫定的に Listの0番目のものを取得
+		// com.linecorp.bot.spring.boot.support.LineMessageHandlerSupportを参考
+		//MessageEvent<TextMessageContent> event = callBackRequest.getEvents().get(0);
+		Event event = callBackRequest.getEvents().get(0);
+		if (event instanceof MessageEvent) {
+			MessageEvent<TextMessageContent> messageEvent= (MessageEvent<TextMessageContent>)event;
+			System.out.println(messageEvent);
+			return lineBotService.makeReply(messageEvent);
+		} else {
+			return null;
+		}
 	}
 
 	@PostMapping(path = "bodyprint")
