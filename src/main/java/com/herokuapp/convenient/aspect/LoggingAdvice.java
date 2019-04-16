6f97ef0cc1f14a5d3ede7e745c262cdaa59ff450
Yuta <uc4w6c@bma.biglobe.ne.jp>
2019-04-16 21:51:14 +0900

    @EventMappingを反映させるためにAOPを解除

--- src/main/java/com/herokuapp/convenient/aspect/LoggingAdvice.java
+++ src/main/java/com/herokuapp/convenient/aspect/LoggingAdvice.java
@@ -23,6 +23,7 @@ public class LoggingAdvice {
 		this.logger = LoggerFactory.getLogger(getClass());
 	}
 
+	/**
 	@Before("execution(* com.herokuapp.convenient.web.*.*(..))")
 	public void invokeApiControllerBefore(JoinPoint joinPoint) {
 		outputLog(joinPoint);
@@ -84,5 +85,6 @@ public class LoggingAdvice {
 	private String getReturnValue(Object returnValue) {
 		return (returnValue != null) ? returnValue.toString() : "return value is null";
 	}
+	**/
 
 }
